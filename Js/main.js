495b22bb894276730a6cc94d8b0b12beb9c28eb3
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-23 13:38:12 -0300

    iniciandi o json no main

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,5 @@
 import {Adedonha} from "./Adedonha.js";
-import questoes from "./json/questoes.json" with { type:"json"}
+import Questoes from "./json/questoes.json" with { type:"json"}
 
 let campo_pergunta=document.getElementById("pergunta");
 let campo_resposta=document.getElementById("resposta");
@@ -7,7 +7,9 @@ let campo_resposta=document.getElementById("resposta");
 const btn_virtual = document.querySelector(".btn_virtual");
 
 const forca= new Adedonha();
+const questoes= Questoes; 
 
+forca.pegandoPergunta(4,questoes);
 
 
 
