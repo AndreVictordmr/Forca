5d7bfd346e0bb2d7a7acf0d3196fb8b1a46a45ea
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-30 14:38:32 -0300

    Atualizando a pagina

--- Js/main.js
+++ Js/main.js
@@ -5,20 +5,49 @@ let campo_pergunta=document.getElementById("pergunta");
 let campo_resposta=document.getElementById("resposta");
 
 const btn_virtual = document.querySelector(".btn_virtual");
+const btn_num = document.querySelector(".btn_num");
+const aviso = document.querySelector("#aviso");
 
 const forca= new Adedonha();
 const questoes= Questoes["perguntas"]; 
 
-forca.starGame(4,questoes);
+btn_num.addEventListener("click", function(){
+  let numb = parseInt(btn_num.textContent);
+  forca.starGame(numb,questoes);
+  atualizarTela();
+});
 
-do{
-campo_pergunta.textContent =forca.perguntas[forca.play_quest].pergunta;
+function atualizarTela(){
+  const mensagem_titulo = aviso.querySelector("h2");
+  const mensagem_pont   = aviso.querySelector("p");
+  if(forca.isPlay){
+    campo_pergunta.textContent =forca.perguntas[forca.play_quest].pergunta.toUpperCase();
+  
+  campo_resposta.textContent = forca.check_resposta.toUpperCase();
+  }else if(forca.isWin){
+      mensagem_titulo.textContent = "Parabem você ganhou";
+      mensagem_pont.textContent = forca.ponto;
+      aviso.showModal();
+  }else{
+      mensagem_titulo.textContent = `Voce chego ate a ${forca.play_quest+1} rodada`;
+      mensagem_pont.textContent = forca.ponto;
+      aviso.showModal();
+  }
+}
 
-campo_resposta.textContent = forca.check_resposta;
+campo_resposta.addEventListener("keyup",(event)=>{verificacao(event.key)});
 
-campo_resposta.addEventListener("keyup");
-
-btn_virtual.addEventListener("click");
+btn_virtual.addEventListener("click",()=>{verificacao(btn_virtual.textContent.trim())});
 
 
-}while(forca.isPlay)
+
+
+function verificacao(letra){
+  if(/^[a-z]$/i.test(letra)){ 
+    if(!forca.erro.includes(letra.toLowerCase())){
+          forca.verificandoResposta(letra);
+      
+      }
+  }  
+  atualizarTela();
+}
